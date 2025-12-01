56f1d319a9552ad2629d21444c8f4f5b47cbdf05
d.martinovicova <P083831@hpcloginresearch01.cluster>
2025-12-01 15:32:01 +0100

    update spapros

--- scripts/spapros/run_spapros.py
+++ scripts/spapros/run_spapros.py
@@ -1,5 +1,16 @@
+#
+# Evaluate Xenium IO probeset panel on NSCLC scRNA-seq dataset
+#
+# Author: Dominika Martinovicova (d.martinovicova@amsterdamumc.nl)
+#
+# Usage:
+#        python run_spapros.py
+
 print('Running run_spapros.py...')
 
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# 0 Import Libraries
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 import pandas as pd
 import scanpy as sc
 import spapros as sp
@@ -10,8 +21,10 @@ sc.settings.verbosity = 1
 sc.logging.print_header()
 print(f"spapros=={sp.__version__}")
 
-
-# Load dataset
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# 1 Load scRNAseq dataset and probeset
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Load data
 adata = sc.read_h5ad('/net/beegfs/groups/tgac/dmartinovicova_new/NSCLC/scRNAseq/data/final_scRNAseq_atlas_Salcher_5k.h5ad')
 print(adata)
 gene_list = pd.read_csv("/net/beegfs/groups/tgac/dmartinovicova_new/DIRECT/data/raw/slide_3/cell_feature_matrix/features.tsv.gz", 
@@ -22,14 +35,12 @@ gene_list = pd.read_csv("/net/beegfs/groups/tgac/dmartinovicova_new/DIRECT/data/
 # Filter out control probes
 controls = ['Codeword', 'Probe']
 controls = "|".join(controls)
-
 gene_list = gene_list[~gene_list[1].str.contains(controls)][1].to_list()
 print(gene_list)
 
 # Check which genes are present in the dataset
 valid_genes = [g for g in gene_list if g in adata.var_names]
 missing = set(gene_list) - set(valid_genes)
-
 print("Missing genes:", missing)
 print("Using", len(valid_genes), "valid genes")
 
@@ -37,7 +48,6 @@ print("Using", len(valid_genes), "valid genes")
 stds = np.std(adata.X.toarray(), axis=0)
 zero_var_genes = np.where(stds == 0)[0]
 zero_var_gene_names = adata.var_names[zero_var_genes]
-
 print("Zero-variance genes:", zero_var_gene_names)
 print("Count:", len(zero_var_genes))
 
@@ -48,11 +58,9 @@ print("Zero-variance genes in the gene list:", zero_var_in_list)
 valid_genes = [g for g in valid_genes if g not in zero_var_gene_names]
 print("Using", len(valid_genes), "valid genes after removing zero-variance genes")
 
-
-
-
-
-
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# 2 Run spapros evaluation
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Set up an Evaluator
 print('Initiating evaluator...')
 evaluator = sp.ev.ProbesetEvaluator(adata, celltype_key='Neutro_Epi_extImm', scheme="full", verbosity=2, results_dir='/net/beegfs/groups/tgac/dmartinovicova_new/DIRECT/results/spapros_5k')
