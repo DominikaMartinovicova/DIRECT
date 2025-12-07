e359c1d219d64627c1fbe0013a0f848154a4f72d
d.martinovicova <P083831@worker03.cluster>
2025-12-07 20:00:15 +0100

    edit cell fraction analysis

--- scripts/analysis_and_plotting/cell_fraction_analysis.py
+++ scripts/analysis_and_plotting/cell_fraction_analysis.py
@@ -59,6 +59,7 @@ sns.color_palette("tab20")
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # 2 Create fractions dataframe
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#Calculate fractions per T_number
 fractions_df = pd.DataFrame(dtype=object)
 for i, element in enumerate(adata.obs['T_number'].unique().dropna()):
     print(f'Processing {i}. T_number: {element}')
@@ -68,23 +69,38 @@ for i, element in enumerate(adata.obs['T_number'].unique().dropna()):
     fractions_df = pd.concat([fractions_df, temp_fractions.rename(element)], axis=1) # Save fractions to df
 
     # Add metadata to the fractions_df
-    for meta in ['sample', 'pt_id', 'sample_type', 'disease_stage', 'T_number', 'regression', 'treatment_scheme']: #'structure',
+    meta_list = ['sample', 'pt_id', 'sample_type', 'disease_stage', 'T_number', 'regression', 'treatment_scheme'] #'structure',
+    for meta in meta_list: 
         fractions_df.loc[meta, element] = adata_temp.obs[meta].unique()[0]
-    
+
+# Adjust dataframe for plotting
 fractions_df = fractions_df.T.fillna(0) # Transpose for easier plotting and fill NaNs with 0
+fractions_df.columns = [f'{col} fraction' if col not in meta_list else col for col in fractions_df.columns] # Add suffix to fraction columns
 fractions_df['MPR'] = np.where(fractions_df['regression']>=90, '>=90', '<90') # Create MPR column
 fractions_df['treatment'] = np.where(fractions_df['treatment_scheme'] == 'v1.7', 'aggressive', 'milder') # Create treatment column
 print(fractions_df.head())
 
+# Keep only patients with matched biopsy and resection samples
+paired_pts = fractions_df['pt_id'][fractions_df['sample_type']=='biopsy'].isin(fractions_df['pt_id'][fractions_df['sample_type']=='resection'])
+paired_fractions_df = fractions_df[fractions_df['pt_id'].isin(paired_pts.index.unique())]
+print(f'Number of paired patients: {len(paired_fractions_df["pt_id"].unique())}')
+
 # Choose analyses to perform
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Analyse shifts in cell fractions before and after treatment
-def celltype_fraction_shifts(adata, fraction_columns, category, output_dir):
+def celltype_fraction_shifts(df, category, output_dir):
+    # Split data into pre- and post-treatment
+    biopsy_df = df[df['sample_type']=='biopsy']
+    resection_df = df[df['sample_type']=='resection']
+    cell_fraction_keys = [col for col in df.columns if col.endswith('fraction')]
+    if category == None:
+           # Do not split into groups, compare biopsy vs resection for all patients
 
 
 
 
 
+celltype_fraction_shifts(paired_fractions_df, )
 # 2. Swarmplots of fractions per chosen category (e.g., structure) with paired pts connected
 #plot_swarmplot(adata_sample_paired, fraction_columns, 'structure', output_plot_dir)
 # 3. Lineplots of fractions per chosen category (e.g., structure) with paired pts connected
