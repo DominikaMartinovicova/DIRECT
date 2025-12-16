ccf5cd27edb8a60c47e8ef393b854fcaf8a7dfe4
d.martinovicova <P083831@hpcloginresearch02.cluster>
2025-12-16 12:02:49 +0100

    remove v1.7 responders vs non responders on a umap

--- scripts/analysis_and_plotting/clustering_responders_vs_non_responders.py
+++ scripts/analysis_and_plotting/clustering_responders_vs_non_responders.py
@@ -8,19 +8,31 @@ adata = sc.read_h5ad('/net/beegfs/groups/tgac/dmartinovicova_new/DIRECT/data/com
 celltype_key = 'Neutro_Epi_extImm'
 output_dir='/net/beegfs/groups/tgac/dmartinovicova_new/DIRECT/plots/combined/Neutro_Epi_extImm/resp_vs_nonresp'
 
+exclude_v17 = True
+if exclude_v17:
+    print('Removing samples with treatment_scheme=v1.7...')
+    adata = adata[adata.obs['treatment_scheme'] != 'v1.7', :]
+    output_dir = output_dir + '/wo_v1.7'
+    print(f'Number of cells after removing v1.7 treatment scheme samples: {adata.shape[0]}')
+
+
+
 # Plot umap with responders vs non-responders
 adata.obs['MPR'] = adata.obs['regression'].apply(lambda x: '>=90' if x >= 90 else '<90')
 sc.pl.umap(adata, color='MPR', show=False)
-plt.savefig(f'{output_dir}/umap_responders_vs_nonresponders.png', dpi=300)
+plt.title('All samples wo v1.7' if exclude_v17 else 'All samples')
+plt.savefig(f'{output_dir}umap_responders_vs_nonresponders.png', dpi=300, bbox_inches='tight')
 plt.close() 
 
 # Subset data for Biopsy and Resection samples
 biopsy_adata = adata[adata.obs['sample_type'] == 'Biopsy']
 sc.pl.umap(biopsy_adata, color='MPR', show=False)
-plt.savefig(f'{output_dir}/umap_responders_vs_nonresponders_biopsy.png', dpi=300)
+plt.title('Biopsy Samples wo v1.7' if exclude_v17 else 'Biopsy Samples')
+plt.savefig(f'{output_dir}umap_responders_vs_nonresponders_biopsy.png', dpi=300, bbox_inches='tight')
 plt.close()
 
 resection_adata = adata[adata.obs['sample_type'] == 'Resection']
 sc.pl.umap(resection_adata, color='MPR', show=False)
-plt.savefig(f'{output_dir}/umap_responders_vs_nonresponders_resection.png', dpi=300)
+plt.title('Resection Samples wo v1.7' if exclude_v17 else 'Resection Samples')
+plt.savefig(f'{output_dir}umap_responders_vs_nonresponders_resection.png', dpi=300, bbox_inches='tight')
 plt.close()
