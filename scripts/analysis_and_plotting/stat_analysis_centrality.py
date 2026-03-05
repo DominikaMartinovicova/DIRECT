e1420cae38e228038be757425cae3e89a6631c06
d.martinovicova <P083831@worker01.cluster>
2026-03-05 16:50:34 +0100

    reformat plotting

--- scripts/analysis_and_plotting/stat_analysis_centrality.py
+++ scripts/analysis_and_plotting/stat_analysis_centrality.py
@@ -553,8 +553,6 @@ else:
     categories = [None, 'MPR', 'treatment']
 
 
-
-
 # loop over different centrality measures
 for key in ['degree_centrality']:             #centrality_scores.keys():    
     if exclude_v17==True:       # filter out v1.7 samples if exclude_v17 is True
@@ -563,24 +561,24 @@ for key in ['degree_centrality']:             #centrality_scores.keys():
         centrality_scores_df = centrality_scores[key]
     centrality_scores_df['sample_id'] = centrality_scores_df.index
 
-    # # Create also dataframe with only paired samples
-    # resection_pts = centrality_scores_df[centrality_scores_df['sample_type']=='Resection']['pt_id'].tolist()
-    # biopsy_pts = centrality_scores_df[centrality_scores_df['sample_type']=='Biopsy']['pt_id'].tolist()
-    # paired_pts = list(set(resection_pts) & set(biopsy_pts))
-    # paired_pt_df = centrality_scores_df[centrality_scores_df['pt_id'].isin(paired_pts)]
+    # Create also dataframe with only paired samples
+    resection_pts = centrality_scores_df[centrality_scores_df['sample_type']=='Resection']['pt_id'].tolist()
+    biopsy_pts = centrality_scores_df[centrality_scores_df['sample_type']=='Biopsy']['pt_id'].tolist()
+    paired_pts = list(set(resection_pts) & set(biopsy_pts))
+    paired_pt_df = centrality_scores_df[centrality_scores_df['pt_id'].isin(paired_pts)]
 
-    # category_map = paired_pt_df[[category, 'pt_id', ]].drop_duplicates() if category in ['MPR', 'treatment'] else paired_pt_df[['pt_id']].drop_duplicates()
-    # centrality_scores_df_paired = paired_pt_df.groupby(['sample_type', 'pt_id'], observed=True).mean(numeric_only=True).reset_index()
+    category_map = paired_pt_df[[category, 'pt_id']].drop_duplicates() if category in ['MPR', 'treatment'] else paired_pt_df[['pt_id']].drop_duplicates()
+    centrality_scores_df_paired = paired_pt_df.groupby(['sample_type', 'pt_id'], observed=True).mean(numeric_only=True).reset_index()
     
-    # pairs_df = pairs_df.merge(category_map, on=['pt_id'], how='left')
-    # print(f'Number of paired patients: {len(pairs_df["pt_id"].unique())}')
+    pairs_df = pairs_df.merge(category_map, on=['pt_id'], how='left')
+    print(f'Number of paired patients: {len(pairs_df["pt_id"].unique())}')
 
     for group in ['sample_type', 'structure', 'structure_core']:
-        print(f'Analyzing {group}')
+        #print(f'Analyzing {group}')
         if group == 'structure':
             centrality_scores_df = centrality_scores_df[centrality_scores_df['sample_type']=='Resection']
         for category in categories:
-            stat_analysis_centrality_scores_box(input_file=centrality_scores_df, output_dir_plots=output_dir_plots, output_dir_results=output_dir_results, group=group, category=category, exclude_v17=exclude_v17, stat_test=mannwhitneyu, cell_type_list=cell_type_list, key=key)
+            #stat_analysis_centrality_scores_box(input_file=centrality_scores_df, output_dir_plots=output_dir_plots, output_dir_results=output_dir_results, group=group, category=category, exclude_v17=exclude_v17, stat_test=mannwhitneyu, cell_type_list=cell_type_list, key=key)
             #if group == 'sample_type':
                 #stat_analysis_centrality_scores_line(input_file=centrality_scores_df_paired, output_dir_plots=output_dir_plots, output_dir_results=output_dir_results, category=category, exclude_v17=exclude_v17, stat_test=wilcoxon, cell_type_list=cell_type_list)
    
@@ -591,7 +589,7 @@ for key in ['degree_centrality']:             #centrality_scores.keys():
                 for single_group in centrality_scores_df[group].unique():
                     print(f'Analyzing category {category} in single group {single_group} in group {group}')
                     centrality_score_df = centrality_scores_df[centrality_scores_df[group]==single_group]
-                    stat_analysis_centrality_scores_box_within_sampletype(input_file=centrality_scores_df, output_dir_plots=output_dir_plots, output_dir_results=output_dir_results, category=category, exclude_v17=exclude_v17, stat_test=mannwhitneyu, cell_type_list=cell_type_list, key=key, single_group=single_group)
+                    #stat_analysis_centrality_scores_box_within_sampletype(input_file=centrality_scores_df, output_dir_plots=output_dir_plots, output_dir_results=output_dir_results, category=category, exclude_v17=exclude_v17, stat_test=mannwhitneyu, cell_type_list=cell_type_list, key=key, single_group=single_group)
     
 
 
